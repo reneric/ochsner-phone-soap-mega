e4bcf2659728402a7e7fb1ed4d2a398a75eb3389
Ren Simmons <rsimmo4@gmail.com>
2019-04-20 21:55:14 -0500

    testing

--- OchsnerPhoneSoapMega.ino
+++ OchsnerPhoneSoapMega.ino
@@ -2,18 +2,24 @@
 #include <Ethernet.h>
 #include <PubSubClient.h>
 
-/************* PRESENCE STATES *************/
-#define ACTIVE_STATE 0
-#define IDLE_STATE 1
+#define DEBUG 1
 
+/********************* PRESENCE STATES ********************/
+#define ACTIVE_STATE 1
+#define IDLE_STATE 0
 
+/*
+ *
+ ** SET THE NUMBER OF PHONE SOAP STATIONS IN THE CLUSTER **
+ *
+ */
 #define NUM_STATIONS 4
 
-/************* MQTT CONNECTION *************/
+/********************* MQTT CONNECTION ********************/
 const int mqttPort = 1883;
 const char* mqttServer = "192.168.2.10";
 
-/************ NETWORK CONNECTION ***********/
+/******************* NETWORK CONNECTION *******************/
 IPAddress PS_T_IP(192, 168, 1, 97);
 byte PS_T_MAC[] = { 0x75, 0xF0, 0x62, 0xC2, 0xAD, 0x09 };
 IPAddress PS_W_IP(192, 168, 1, 98);
@@ -24,7 +30,7 @@ byte PS_O_MAC[] = { 0x44, 0xA0, 0x99, 0x11, 0xFA, 0x93 };
 IPAddress ip;
 byte mac[sizeof(PS_T_MAC)];
 
-/*************** MQTT TOPICS ***************/
+/*********************** MQTT TOPICS **********************/
 const char* T1_TOPIC = "phoneSoap/PS_T1";
 const char* T2_TOPIC = "phoneSoap/PS_T2";
 const char* T3_TOPIC = "phoneSoap/PS_T3";
@@ -40,23 +46,34 @@ const char* O2_TOPIC = "phoneSoap/PS_O2";
 const char* O3_TOPIC = "phoneSoap/PS_O3";
 const char* O4_TOPIC = "phoneSoap/PS_O4";
 
+
+/******** Initialize phone soap station topics ***********/
 char* STATION;
 char PS1_TOPIC;
 char PS2_TOPIC;
 char PS3_TOPIC;
 char PS4_TOPIC;
 
-/******** MQTT PHONE SOAP STATIONS *********/
+
+
 /*
- * 
- * *** SET STATION BELOW ***
- * ****** THIS IS REQUIRED ******
+ * MQTT PHONE SOAP STATIONS
+ *
+ * Set station below (REQUIRED)
  *
  */
 const char* STATION_T = "PS_T";
 // const char* STATION_W = "PS_W";
 // const char* STATION_O = "PS_O";
 
+
+/*
+ *
+ * Here, we set the configuration for the phone soap cluster.
+ * This will choose the IP and MAC addresses as well as set 
+ * the MQTT topics used for broadcasting states.
+ *
+ */
 #if defined(STATION_T)
   STATION = STATION_T;
   ip = PS_T_IP;
@@ -85,46 +102,54 @@ const char* STATION_T = "PS_T";
 
 
 
-/************** STATION PINS **************/
-const int PS1_sensorPin = 23;          // The phone soap input pin for PS1 (INPUT)
-const int PS1_activePin = 22;          // The ACTIVE state pin to trigger the PS1 matrix/teensy (OUTPUT)
+/********************** STATION PINS *********************/
+const int PS1_sensorPin = 23; // The phone soap input pin for PS1 (INPUT)
+const int PS1_activePin = 22; // The ACTIVE state pin to trigger the PS1 matrix/teensy (OUTPUT)
 
-const int PS2_sensorPin = 25;          // The phone soap input pin for PS2 (INPUT)
-const int PS2_activePin = 24;          // The ACTIVE state pin to trigger the PS2 matrix/teensy (OUTPUT)
+const int PS2_sensorPin = 25; // The phone soap input pin for PS2 (INPUT)
+const int PS2_activePin = 24; // The ACTIVE state pin to trigger the PS2 matrix/teensy (OUTPUT)
 
-const int PS3_sensorPin = 27;          // The phone soap input pin for PS3 (INPUT)
-const int PS3_activePin = 26;          // The ACTIVE state pin to trigger the PS3 matrix/teensy (OUTPUT)
+const int PS3_sensorPin = 27; // The phone soap input pin for PS3 (INPUT)
+const int PS3_activePin = 26; // The ACTIVE state pin to trigger the PS3 matrix/teensy (OUTPUT)
 
-const int PS4_sensorPin = 29;          // The phone soap input pin for PS4 (INPUT)
-const int PS4_activePin = 28;          // The ACTIVE state pin to trigger the PS4 matrix/teensy (OUTPUT)
+const int PS4_sensorPin = 29; // The phone soap input pin for PS4 (INPUT)
+const int PS4_activePin = 28; // The ACTIVE state pin to trigger the PS4 matrix/teensy (OUTPUT)
 
 
-/******** INITIALIZE CURRENT STATE ********/
+/**************** INITIALIZE CURRENT STATE ***************/
 int PS1_currentState;
 int PS2_currentState;
 int PS3_currentState;
 int PS4_currentState;
 
 
-/******* INITIALIZE ETHERNET LIBRARY *******/
+
+/************** INITIALIZE ETHERNET LIBRARY *************/
 EthernetClient net;
-/********* INITIALIZE MQTT LIBRARY *********/
+
+/**************** INITIALIZE MQTT LIBRARY ***************/
 PubSubClient mqttClient(net);
 
-// Station names, used as MQTT Topics
+
+
+/********* STATION NAMES (used as MQTT Topics) **********/
 const char stations[NUM_STATIONS][10] = {PS1_TOPIC, PS2_TOPIC, PS3_TOPIC, PS4_TOPIC};
 
-// Station states, used as MQTT Messages
-const char states[2][10] = {"ACTIVE", "IDLE"};
+/******* STATION STATES (used as MQTT Messages) *********/
+const char states[2][10] = {"IDLE", "ACTIVE"};
 
-// Put the current states into an array for indexing
+
+
+/****** Put the states into an array for indexing *******/
 int currentStates[NUM_STATIONS] = {PS1_currentState, PS2_currentState, PS3_currentState, PS4_currentState};
 
-// Put the pins into arrays for indexing
+/******* Put the pins into an array for indexing ********/
 const int sensorPins[NUM_STATIONS] = {PS1_sensorPin, PS2_sensorPin, PS3_sensorPin, PS4_sensorPin};
 const int activePins[NUM_STATIONS] = {PS1_activePin, PS2_activePin, PS3_activePin, PS4_activePin};
 
-// Reconnect to the MQTT broker when the connection is lost
+
+
+/*************** RECONNECT TO MQTT BROKER ***************/
 void reconnect() {
   while (!mqttClient.connected()) {
     Serial.print("Attempting MQTT connection...");
@@ -142,12 +167,14 @@ void reconnect() {
         Serial.print("failed, rc=");
         Serial.print(mqttClient.state());
         Serial.println(" try again in 5 seconds");
-        // Wait 5 seconds before retrying
         delay(5000);
     }
   }
 }
 
+
+/*********** RECONNECT WITHOUT BLOCKING LOOP ************/
+long lastReconnectAttempt = 0;
 boolean reconnect_non_blocking() {
   if (mqttClient.connect(STATION)) {
     Serial.println("Connected!");
@@ -165,6 +192,7 @@ boolean reconnect_non_blocking() {
   return mqttClient.connected();
 }
 
+/**** CALLBACK RUN WHEN AN MQTT MESSAGE IS RECEIVED *****/
 void messageReceived(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);  
@@ -175,43 +203,57 @@ void messageReceived(char* topic, byte* payload, unsigned int length) {
   {
     payloadArr[i] = (char)payload[i];
   }
+
+  // Terminate array with null value
   payloadArr[length] = 0;
 
-  Serial.println(payloadArr);  // null terminated array
+  Serial.println(payloadArr);\
 }
 
-long lastReconnectAttempt = 0;
 void setup() {
   // Initialize serial communication:
   Serial.begin(9600);
 
   // Initialize the ethernet connection
   Ethernet.begin(mac, ip);
-  mqttClient.setServer(mqttServer, 1883);
+  // Serial.print("Ethernet connection initialized with IP Address: ");
+  // Serial.print(ip);
+  // Serial.print(", and MAC Address: ");
+  // Serial.println(mac);
+
+  // Initialize the MQTT connection
+  mqttClient.setServer(mqttServer, mqttPort);
   mqttClient.setCallback(messageReceived);
-  
+  Serial.print("MQTT connection initialized on MQTT Server: ");
+  Serial.print(mqttServer);
+  Serial.print(", on port: ");
+  Serial.println(mqttPort);
+
+
   // Initialize all pins and set currentStates to IDLE
   for (int i = 0; i < NUM_STATIONS; i++) {
-    pinMode(sensorPins[i], INPUT);
+    pinMode(sensorPins[i], INPUT_PULLUP);
     pinMode(activePins[i], OUTPUT);
     currentStates[i] = IDLE_STATE;
   }
+  pinMode(PS1_sensorPin, INPUT);
+  pinMode(PS1_activePin, OUTPUT);
   lastReconnectAttempt = 0;
 }
 
 void loop() {
-  if (!mqttClient.connected()) {
-    long now = millis();
-    if (now - lastReconnectAttempt > 5000) {
-      lastReconnectAttempt = now;
-      // Attempt to reconnect
-      if (reconnect_non_blocking()) {
-        lastReconnectAttempt = 0;
-      }
-    }
-  } else {
-    mqttClient.loop();
-  }
+  // if (!mqttClient.connected()) {
+  //   long now = millis();
+  //   if (now - lastReconnectAttempt > 5000) {
+  //     lastReconnectAttempt = now;
+  //     // Attempt to reconnect
+  //     if (reconnect_non_blocking()) {
+  //       lastReconnectAttempt = 0;
+  //     }
+  //   }
+  // } else {
+  //   mqttClient.loop();
+  // }
 
   // Run each statin through the state machine
   for (int i = 0; i < NUM_STATIONS; i++) {
@@ -221,11 +263,19 @@ void loop() {
   delay(100);
 }
 
-int lastTempState[NUM_STATIONS];
+int tempState[NUM_STATIONS];
 void stateMachine (int pos) {
   // Get the current sensor state
-  lastTempState[pos] = getState(pos);
-  
+  tempState[pos] = getState(pos);
+  int currentState = currentStates[pos]; 
+#if DEBUG == 1
+  if (pos == 0) {
+    Serial.print("PS1 state: ");
+    Serial.println(getState(pos));
+    Serial.println(states[tempState[pos]]);
+    Serial.println("");
+  }
+#endif
   /*
    * Only send the state update on the first loop.
    *
@@ -233,18 +283,20 @@ void stateMachine (int pos) {
    * THEN we can safely change the actual state and broadcast it.
    *
    */
-  if (lastTempState[pos] != currentStates[pos]) {
-#if DEBUG == 1    
-    Serial.print("Sensor State Changed: ");
-    Serial.println(stations[pos]);
-    Serial.print("Last State: ");
-    Serial.println(currentStates[pos]);
-    Serial.print("New State: ");
-    Serial.println(lastTempState[pos]);
+  if (tempState[pos] != currentState) {
+#if DEBUG == 1
+    if (pos == 0) {
+      Serial.print("Sensor State Changed: ");
+      Serial.println(stations[pos]);
+      Serial.print("Last State: ");
+      Serial.println(currentState);
+      Serial.print("New State: ");
+      Serial.println(tempState[pos]);
+    }
 #endif
-    currentStates[pos] = lastTempState[pos];
+    currentStates[pos] = tempState[pos];
     // Publish the message for this station. i.e. client.publish("PS1", "ACTIVE")
-    mqttClient.publish(stations[pos], states[currentStates[pos]]);
+    if (mqttClient.connected()) mqttClient.publish(stations[pos], states[currentStates[pos]]);
   }
   switch (currentStates[pos]) {
     case ACTIVE_STATE:
@@ -256,14 +308,33 @@ void stateMachine (int pos) {
   }
 }
 
+/********* SET GIVEN STATION PIN TO ACTIVE *********/
 void setActive (int pos) {
+#if DEBUG == 1
+  if (pos == 0) {
+    Serial.println("Set ACTIVE: PS1");
+  }
+#endif
   digitalWrite(activePins[pos], HIGH);
 }
 
+/********** SET GIVEN STATION PIN TO IDLE **********/
 void setIdle (int pos) {
+#if DEBUG == 1
+  if (pos == 0) {
+    Serial.println("Set IDLE: PS1");
+  }
+#endif
   digitalWrite(activePins[pos], LOW);
 }
 
+/****** GET STATE OF STATIONS PHONE SOAP PIN *******/
 int getState(int pos) {
-  return digitalRead(sensorPins[pos]) == HIGH ? ACTIVE_STATE : IDLE_STATE;
+#if DEBUG == 1
+  if (pos == 0) {
+    Serial.print("PS1 Input: ");
+    Serial.println(digitalRead(sensorPins[pos]) == HIGH);
+  }
+#endif
+  return digitalRead(sensorPins[pos]) == HIGH;
 }
